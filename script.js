7d8cadb517dc38e88aa600417783921dd8a3b03d
Adnan <102221244+losingsan3ty@users.noreply.github.com>
2022-10-02 20:37:01 +0300

    smooth scrolling

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const mobileNav = function () {
   const navList = document.querySelector(".nav-list");
   const menuBtn = document.querySelector(".menu-btn");
   const navBtnContainer = document.querySelector(".nav-mobile");
+  const navToggle = function () {
+    navList.classList.toggle("nav-list-active");
+    sections.forEach((sec) => sec.classList.toggle("nav-open"));
+  };
   let menuOpen = false;
   navBtnContainer.addEventListener("click", () => {
     if (!menuOpen) {
@@ -12,8 +16,16 @@ const mobileNav = function () {
       menuBtn.classList.remove("open");
       menuOpen = false;
     }
-    navList.classList.toggle("nav-list-active");
-    sections.forEach((sec) => sec.classList.toggle("nav-open"));
+    navToggle();
+  });
+  // smooth scrolling
+  navList.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (!e.target.classList.contains("nav-link")) return;
+    const sectionId = e.target.getAttribute("href").split("#")[1];
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: "smooth" });
+    navToggle();
   });
 };
 mobileNav();
