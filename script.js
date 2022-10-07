fa5ce213d38384d83f3f1650af94ada4c8f24807
Adnan <102221244+losingsan3ty@users.noreply.github.com>
2022-10-07 12:21:19 +0300

    almost finished

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const sections = document.querySelectorAll(".section");
-const navList = document.querySelector(".mobile-nav-list");
+const navList = document.querySelectorAll(".nav-list");
+const mobileNavList = document.querySelector(".mobile-nav-list");
 const navCloseBtn = document.querySelector(".btn-close-mobile-nav");
 const menuBtn = document.querySelector(".menu-btn");
 const navMobileBtn = document.querySelector(".btn-mobile-nav");
 
 const mobileNavToggle = function () {
   // move the body and show the nav
-  navList.classList.toggle("mobile-nav-list-active");
+  mobileNavList.classList.toggle("mobile-nav-list-active");
   sections.forEach((sec) => sec.classList.toggle("nav-open"));
   // hide the nav menu button
   menuBtn.classList.toggle("open");
@@ -19,14 +20,18 @@ navCloseBtn.addEventListener("click", function (e) {
   mobileNavToggle();
 });
 // smooth scrolling
-navList.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (!e.target.classList.contains("nav-link")) return;
-  const sectionId = e.target.getAttribute("href").split("#")[1];
-  const section = document.getElementById(sectionId);
-  section.scrollIntoView({ behavior: "smooth" });
-  navToggle();
-  menuToggle();
+navList.forEach((list) => {
+  list.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (!e.target.classList.contains("nav-link")) return;
+    const sectionId = e.target.getAttribute("href").split("#")[1];
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: "smooth" });
+    if (list.classList.contains("main-nav-list")) {
+      return;
+    }
+    mobileNavToggle();
+  });
 });
 // section reveal
 
@@ -64,7 +69,9 @@ const imgTargets = document.querySelectorAll("img[data-src]");
 const loadImg = function (entries, observer) {
   const [entry] = entries;
 
-  if (!entry.isIntersecting) return;
+  if (!entry.isIntersecting) {
+    return;
+  }
 
   // Replace src with data-src
   entry.target.src = entry.target.dataset.src;
@@ -75,7 +82,7 @@ const loadImg = function (entries, observer) {
 
   observer.unobserve(entry.target);
 };
-
+const initIntersection = function () {};
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
